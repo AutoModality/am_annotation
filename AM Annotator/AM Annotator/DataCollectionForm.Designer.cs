f3217c1f523bc4f27ea90dda7586cbd1ad73ffcb
Unknown <ajanani85@gmail.com>
2019-02-11 11:29:16 -0800

    Completed

--- AM Annotator/AM Annotator/DataCollectionForm.Designer.cs
+++ AM Annotator/AM Annotator/DataCollectionForm.Designer.cs
@@ -36,17 +36,27 @@
             this.Project = new System.Windows.Forms.ToolStripDropDownButton();
             this.annotatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataCollectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectOrganizerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripDropDownButton3 = new System.Windows.Forms.ToolStripDropDownButton();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.refreshBTN = new System.Windows.Forms.Button();
             this.devicesLB = new System.Windows.Forms.ListBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.projectOrganizerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchedImageLV = new System.Windows.Forms.ListView();
+            this.searchTB = new System.Windows.Forms.TextBox();
+            this.searchBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputImageTB = new System.Windows.Forms.TextBox();
+            this.setOutputImageBTN = new System.Windows.Forms.Button();
+            this.numberOfImagesNUD = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.cameraPB)).BeginInit();
             this.toolStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfImagesNUD)).BeginInit();
             this.SuspendLayout();
             // 
             // cameraPB
@@ -105,15 +115,22 @@
             // annotatorToolStripMenuItem
             // 
             this.annotatorToolStripMenuItem.Name = "annotatorToolStripMenuItem";
-            this.annotatorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.annotatorToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
             this.annotatorToolStripMenuItem.Text = "Annotator";
             this.annotatorToolStripMenuItem.Click += new System.EventHandler(this.annotatorToolStripMenuItem_Click);
             // 
             // dataCollectionToolStripMenuItem
             // 
             this.dataCollectionToolStripMenuItem.Name = "dataCollectionToolStripMenuItem";
-            this.dataCollectionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dataCollectionToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
             this.dataCollectionToolStripMenuItem.Text = "Data Collection";
+            // 
+            // projectOrganizerToolStripMenuItem
+            // 
+            this.projectOrganizerToolStripMenuItem.Name = "projectOrganizerToolStripMenuItem";
+            this.projectOrganizerToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.projectOrganizerToolStripMenuItem.Text = "Project Organizer";
+            this.projectOrganizerToolStripMenuItem.Click += new System.EventHandler(this.projectOrganizerToolStripMenuItem_Click);
             // 
             // toolStripDropDownButton3
             // 
@@ -167,6 +184,14 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.numberOfImagesNUD);
+            this.tabPage2.Controls.Add(this.setOutputImageBTN);
+            this.tabPage2.Controls.Add(this.outputImageTB);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.searchBTN);
+            this.tabPage2.Controls.Add(this.searchTB);
+            this.tabPage2.Controls.Add(this.searchedImageLV);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -175,12 +200,80 @@
             this.tabPage2.Text = "Google Image";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // projectOrganizerToolStripMenuItem
+            // searchedImageLV
             // 
-            this.projectOrganizerToolStripMenuItem.Name = "projectOrganizerToolStripMenuItem";
-            this.projectOrganizerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.projectOrganizerToolStripMenuItem.Text = "Project Organizer";
-            this.projectOrganizerToolStripMenuItem.Click += new System.EventHandler(this.projectOrganizerToolStripMenuItem_Click);
+            this.searchedImageLV.Location = new System.Drawing.Point(387, 6);
+            this.searchedImageLV.Name = "searchedImageLV";
+            this.searchedImageLV.Size = new System.Drawing.Size(608, 451);
+            this.searchedImageLV.TabIndex = 0;
+            this.searchedImageLV.UseCompatibleStateImageBehavior = false;
+            // 
+            // searchTB
+            // 
+            this.searchTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchTB.Location = new System.Drawing.Point(6, 470);
+            this.searchTB.Name = "searchTB";
+            this.searchTB.Size = new System.Drawing.Size(950, 31);
+            this.searchTB.TabIndex = 1;
+            // 
+            // searchBTN
+            // 
+            this.searchBTN.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("searchBTN.BackgroundImage")));
+            this.searchBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.searchBTN.Location = new System.Drawing.Point(962, 468);
+            this.searchBTN.Name = "searchBTN";
+            this.searchBTN.Size = new System.Drawing.Size(33, 33);
+            this.searchBTN.TabIndex = 2;
+            this.searchBTN.UseVisualStyleBackColor = true;
+            this.searchBTN.Click += new System.EventHandler(this.searchBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Image Locations: ";
+            // 
+            // outputImageTB
+            // 
+            this.outputImageTB.Enabled = false;
+            this.outputImageTB.Location = new System.Drawing.Point(104, 27);
+            this.outputImageTB.Name = "outputImageTB";
+            this.outputImageTB.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.outputImageTB.Size = new System.Drawing.Size(277, 20);
+            this.outputImageTB.TabIndex = 4;
+            // 
+            // setOutputImageBTN
+            // 
+            this.setOutputImageBTN.Location = new System.Drawing.Point(104, 54);
+            this.setOutputImageBTN.Name = "setOutputImageBTN";
+            this.setOutputImageBTN.Size = new System.Drawing.Size(277, 23);
+            this.setOutputImageBTN.TabIndex = 5;
+            this.setOutputImageBTN.Text = "Set";
+            this.setOutputImageBTN.UseVisualStyleBackColor = true;
+            // 
+            // numberOfImagesNUD
+            // 
+            this.numberOfImagesNUD.Location = new System.Drawing.Point(104, 120);
+            this.numberOfImagesNUD.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numberOfImagesNUD.Name = "numberOfImagesNUD";
+            this.numberOfImagesNUD.Size = new System.Drawing.Size(277, 20);
+            this.numberOfImagesNUD.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Number of Images:";
             // 
             // DataCollectionForm
             // 
@@ -199,6 +292,9 @@
             this.toolStrip1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfImagesNUD)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,5 +316,13 @@
         private System.Windows.Forms.ListBox devicesLB;
         private System.Windows.Forms.Button refreshBTN;
         private System.Windows.Forms.ToolStripMenuItem projectOrganizerToolStripMenuItem;
+        private System.Windows.Forms.Button searchBTN;
+        private System.Windows.Forms.TextBox searchTB;
+        private System.Windows.Forms.ListView searchedImageLV;
+        private System.Windows.Forms.TextBox outputImageTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button setOutputImageBTN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numberOfImagesNUD;
     }
 }
--- AM Annotator/AM Annotator/DataCollectionForm.cs
+++ AM Annotator/AM Annotator/DataCollectionForm.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using Emgu.CV.Ocl;
+using System.IO;
+using System.Net;
 
 namespace AM_Annotator
 {
@@ -76,6 +78,124 @@ namespace AM_Annotator
             t.SetApartmentState(System.Threading.ApartmentState.STA);
             t.Start();
             this.Close();
+        }
+
+        private void searchBTN_Click(object sender, EventArgs e)
+        {
+            //Check for Errors
+            string error_str = "";
+            if (searchTB.Text == "")
+            {
+                error_str += "No search string. \n";
+            }
+            if (NetwrokIsDown())
+            {
+                error_str += "Network is down. Check your connnection.\n";
+            }
+            if (numberOfImagesNUD.Value <= 0)
+            {
+                error_str += "Number of images should be valid.\n";
+            }
+            if (outputImageTB.Text == "" || !Directory.Exists(outputImageTB.Text))
+            {
+                error_str += "Enter a valid path for outputw.\n";
+            }
+            if (error_str != "")
+            {
+                MessageBox.Show(error_str, "Failed to Search");
+                return;
+            }
+
+
+            //Getting the images
+            var urls = GetUrls(searchTB.Text);
+            for (int img_cnt = 0; img_cnt < numberOfImagesNUD.Value; img_cnt++)
+            {
+                GetImage(urls[img_cnt]);
+            }
+        }
+
+        /// <summary>
+        /// A routine to see if the network is down
+        /// </summary>
+        /// <returns></returns>
+        private bool NetwrokIsDown()
+        {
+            return System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+        }
+
+
+        /// <summary>
+        /// Getting all the urls from the given search keywords
+        /// </summary>
+        /// <param name="topic"></param>
+        /// <returns></returns>
+        private List<string> GetUrls(string topic)
+        {
+            List<string> urls = new List<string>();
+
+            //setting up the search string
+            topic.Replace(' ', '+');
+
+            string url = "https://www.google.com/search?q=" + topic + "&tbm=isch";
+            
+            string htmlResponse = "";
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+            //receiving the html response
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                if (dataStream == null)
+                {
+                    MessageBox.Show("Something went wrong", "Failed to obtain response");
+                }
+                using (var sr = new StreamReader(dataStream))
+                {
+                    htmlResponse = sr.ReadToEnd();
+                }
+            }
+
+
+            //separating the urls
+            int ndx = htmlResponse.IndexOf("\"ou\"", StringComparison.Ordinal);
+
+            while (ndx >= 0)
+            {
+                ndx = htmlResponse.IndexOf("\"", ndx + 4, StringComparison.Ordinal);
+                ndx++;
+                int ndx2 = htmlResponse.IndexOf("\"", ndx, StringComparison.Ordinal);
+                string img_url = htmlResponse.Substring(ndx, ndx2 - ndx);
+                urls.Add(url);
+                ndx = htmlResponse.IndexOf("\"ou\"", ndx2, StringComparison.Ordinal);
+            }
+            return urls;
+        }
+
+        /// <summary>
+        /// Getting an image from the passed url
+        /// </summary>
+        /// <param name="url">url to the image</param>
+        /// <returns></returns>
+        private Image GetImage(string url)
+        {
+            Image result;
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                if (dataStream == null)
+                    return null;
+                using (var sr = new BinaryReader(dataStream))
+                {
+                    byte[] bytes = sr.ReadBytes(100000);
+                    using (var ms = new MemoryStream(bytes))
+                    {
+                        result = Image.FromStream(ms);
+                    }
+                }
+            }
+            return result;
         }
     }
 }
